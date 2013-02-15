de822352b40c158b1bae174e4f2f37386928341d
Jon R. Humphrey <jon@randomprecision.co.uk>
2013-02-15 10:08:46 +0000

    removed instance paths

--- SetUmbracoPermissions.bat
+++ SetUmbracoPermissions.bat
@@ -3,7 +3,6 @@ set /p UserAppPoolName= Please enter the Application Pool Name you would like to
 
 REM Following line in original script incorrectly sets all child folder permissions
 REM icacls . /grant %UserAppPoolName%:(OI)(CI)M
-icacls .sass-cache /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls app_browsers /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)RX
 icacls app_code /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)RX
 icacls app_data /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
@@ -14,13 +13,10 @@ icacls css /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls data /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls fonts /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)RX
 icacls images /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
-icacls js /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls macroscripts /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls masterpages /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls media /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
-icacls sass /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls scripts /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
-icacls stylesheets /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls umbraco /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
 icacls usercontrols /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)R
 icacls xslt /grant "IIS APPPOOL\%UserAppPoolName%":(OI)(CI)M
